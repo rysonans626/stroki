fb11371341715ff83d31f99bffc8c4de3795a418
aorandomcrew <danbogomolov@gmail.com>
2022-06-01 11:20:10 +0300

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-
-    }
+//задание 1-2
+    String firstName = "Ivan ";
+    String middleName = "Ivanovich ";
+    String lastName = "Ivanov ";
+    String fullName = lastName + firstName + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+    String result = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + result);
+    zadanie3();}
+//задание 3
+public static void zadanie3(){
+        String fullName = "Иванов Семён Семёнович";
+    fullName = fullName.replace("ё", "е");
+    System.out.println("Данные ФИО сотрудника — " + fullName);
+}
 }
